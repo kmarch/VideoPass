911992c36a932cea7bf751a48299ae184fde68e2
SimkaDev <sion.romain@live.fr>
2014-10-24 16:50:28 +0200

    ServiceDVD

---
+++ grails-app/services/videopass/DVDService.groovy
@@ -0,0 +1,46 @@
+package videopass
+
+import IVVQ.DVDs.DVD
+import org.springframework.transaction.annotation.Transactional
+
+@Transactional
+class DVDService {
+
+    def getListeDVD() {
+        DVD.all
+    }
+
+    def ajoutDVD(String titre, long duree, Genre genre, int nb) {
+        DVD dvd = new DVD(titre: titre, duree: duree, genre : genre, nbExemplaire: nb)
+        int id = 0
+        if (!DVD.all.isEmpty()) {
+            id = 1
+        }
+        dvd.id = id
+        dvd.sortie = new Date().getTime()
+        dvd.save()
+        dvd
+    }
+
+    def getDVD(String name) {
+        def dvd = DVD.findByTitre(name)
+        dvd
+    }
+
+    def supprimerDVD(int id) {
+        def dvd = DVD.findById(id)
+        dvd.delete()
+    }
+
+    def getNouveaute() {
+        def dvds = DVD.all()
+        List newDvd
+        def dateMin = new Date().getTime()-(1000*60*60*24*14)
+        for (DVD dvd : dvds) {
+            if (dvd.getSortie() > dateMin) {
+                newDvd.add(dvd)
+            }
+        }
+        newDvd
+    }
+}
