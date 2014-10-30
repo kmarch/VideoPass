dcf409d3a3938f9099914b1203a54819f4889c16
kmarch <kevinmarchois@gmail.com>
2014-10-30 08:52:49 +0100

    #1 Suppression idUtilisateur dans le domaine et dans les tests

--- test/integration/IVVQ/utilisateurs/UtilisateurIntegrationSpec.groovy
+++ test/integration/IVVQ/utilisateurs/UtilisateurIntegrationSpec.groovy
@@ -10,7 +10,6 @@ class UtilisateurIntegrationSpec extends Specification  {
     Utilisateur jumeau
     def setup() {
         utilisateur = new Utilisateur()
-        utilisateur.idUtil = "007"
         utilisateur.isAdmin = false
         utilisateur.pseudo = "Methos"
         utilisateur.mdp = "azerty"
@@ -21,24 +20,9 @@ class UtilisateurIntegrationSpec extends Specification  {
     def cleanup() {
     }
 
-    void "test ajout de deux utilisateurs ayant le même id"() {
-        given:"le même utilisateur"
-        jumeau = new Utilisateur()
-        jumeau.idUtil = "007"
-        jumeau.isAdmin = false
-        jumeau.pseudo = "Lol"
-        jumeau.mdp = "azerty"
-        jumeau.dateNaissance = new Date().time - 86400000
-        when:"ajout de lui même"
-        def estBon = jumeau.save()
-        then:"ça plante"
-        !estBon
-    }
-
     void "test ajout de deux utilisateurs ayant le même pseudo"() {
         given:"le même pseudo"
         jumeau = new Utilisateur()
-        jumeau.idUtil = "008"
         jumeau.isAdmin = false
         jumeau.pseudo = "Methos"
         jumeau.mdp = "azerty"
