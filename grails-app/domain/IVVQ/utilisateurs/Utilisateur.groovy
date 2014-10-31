49318d92ff1961c69c0d277c1e8a5ebc1b191047
kmarch <kevinmarchois@gmail.com>
2014-10-31 10:46:01 +0100

    #9 Ajout des liens entre Utilisateur, Commentaire et SousCommentaire, plus tests intégrations suppression en cascade

--- grails-app/domain/IVVQ/utilisateurs/Utilisateur.groovy
+++ grails-app/domain/IVVQ/utilisateurs/Utilisateur.groovy
@@ -1,15 +1,20 @@
 package IVVQ.utilisateurs
 
+import IVVQ.social.Commentaire
+import IVVQ.social.SousCommentaire
+
 class Utilisateur {
 
     boolean isAdmin
     String pseudo
     String mdp
     long dateNaissance
+    static hasMany = [listeCommentaires:Commentaire, listeSousCommentaires:SousCommentaire]
 
     static constraints = {
         pseudo nullable:false, blank:false, unique:true
         mdp nullable:false, blank:false
         dateNaissance max:new Date().time
     }
+
 }
--- test/integration/IVVQ/utilisateursServices/UtilisateurServiceSpec.groovy
+++ test/integration/IVVQ/utilisateursServices/UtilisateurServiceSpec.groovy
@@ -1,7 +1,11 @@
 package IVVQ.utilisateursServices
 
+import IVVQ.DVDs.DVD
+import IVVQ.social.Commentaire
+import IVVQ.social.SousCommentaire
 import IVVQ.utilisateurs.Utilisateur
 import spock.lang.*
+import videopass.Genre
 
 /**
  *
@@ -25,13 +29,12 @@ class UtilisateurServiceSpec extends Specification {
     }
 
     def cleanup() {
-        Utilisateur.all.each {it -> it.delete()}
     }
 
     void "test d'initialisation de la liste"() {
         given: "une base de données avec deux utilisateurs"
-        utilisateur1.save(flush:true, failOnError:true)
-        utilisateur2.save(flush:true, failOnError:true)
+        utilisateur1.save(flush: true, failOnError: true)
+        utilisateur2.save(flush: true, failOnError: true)
 
         when: "on initialise la liste"
         service.initialiserListeUtilisateurs()
@@ -88,5 +91,76 @@ class UtilisateurServiceSpec extends Specification {
 
         then: "l'utilisateur récupéré est bien celui demandé"
         res.pseudo == utilisateur1.pseudo
+    }
+
+    void "Test Ajout Commentaire et SousCommentaire"() {
+        given: "Un utilisateur valide et des Commentaire et SousCommentaire et un DVD"
+        Utilisateur utilisateur = new Utilisateur()
+        DVD dvd = new DVD()
+        Commentaire commentaire1 = new Commentaire()
+        SousCommentaire sousCommentaire = new SousCommentaire()
+        utilisateur.isAdmin = false
+        utilisateur.pseudo = "Madara"
+        utilisateur.mdp = "azerty"
+        utilisateur.dateNaissance = new Date().time - 86400000
+        dvd.duree = 120
+        dvd.genre = Genre.action
+        dvd.nbExemplaire = 12
+        dvd.titre = "La mort au trousse"
+        dvd.sortie = new Date().time - 86400000
+        commentaire1.utilisateur = utilisateur
+        commentaire1.commentaire = "Bien"
+        commentaire1.date = new Date().time + 86400000
+        commentaire1.dvd = dvd
+        sousCommentaire.commentaire = commentaire1
+        sousCommentaire.dateC = new Date().time - 86400000
+        sousCommentaire.texteCommentaire = "J'aime aussi"
+        sousCommentaire.utilisateur = utilisateur
+        dvd.save(failOnError: true)
+        when: "Ajoute les Commentaires et SousCommentaire"
+        utilisateur.addToListeSousCommentaires(sousCommentaire)
+        utilisateur.addToListeCommentaires(commentaire1)
+        utilisateur.save()
+        service.ajoutUtilisateur(utilisateur)
+
+        then: "On a 1 Commentaires et 1 SousCommentaire"
+        utilisateur.listeSousCommentaires.size() == 1
+        utilisateur.listeCommentaires.size() == 1
+    }
+
+    void "Test Suppression en cascade d'un utilisateur"() {
+        given: "Un utilisateur valide et des Commentaire et SousCommentaire et un DVD"
+        Utilisateur utilisateur = new Utilisateur()
+        DVD dvd = new DVD()
+        Commentaire commentaire1 = new Commentaire()
+        SousCommentaire sousCommentaire = new SousCommentaire()
+        utilisateur.isAdmin = false
+        utilisateur.pseudo = "Methos"
+        utilisateur.mdp = "azerty"
+        utilisateur.dateNaissance = new Date().time - 86400000
+        dvd.duree = 120
+        dvd.genre = Genre.action
+        dvd.nbExemplaire = 12
+        dvd.titre = "La mort au trousse"
+        dvd.sortie = new Date().time - 86400000
+        commentaire1.utilisateur = utilisateur
+        commentaire1.commentaire = "Bien"
+        commentaire1.date = new Date().time + 86400000
+        commentaire1.dvd = dvd
+        dvd.save()
+        service.ajoutUtilisateur(utilisateur)
+        sousCommentaire.commentaire = commentaire1
+        sousCommentaire.dateC = new Date().time - 86400000
+        sousCommentaire.texteCommentaire = "J'aime aussi"
+        sousCommentaire.utilisateur = utilisateur
+        utilisateur.addToListeSousCommentaires(sousCommentaire)
+        utilisateur.addToListeCommentaires(commentaire1)
+        utilisateur.save()
+        when: "On supprime l'utilisateur"
+        (service.supprimerUtilisateur(utilisateur.pseudo))
+        then: "On a aucun Commentaire et SousCommentaire en BD"
+        Commentaire.all.size() == 0
+        SousCommentaire.all.size() == 0
+
     }
 }
